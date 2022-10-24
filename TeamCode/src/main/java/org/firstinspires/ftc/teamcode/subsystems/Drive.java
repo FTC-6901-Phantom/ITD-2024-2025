990d9b74134ebb281e93bf51b8c7cd8f7257754d
andrea <andreangonzalez59@gmail.com>
2022-10-24 13:04:23 -0500

    Made a slow speed and made constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -8,10 +8,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Drive {
 
+    public static double SLOW_SPEED = 0.3;
+    public static double NORMAL_SPEED= 1;
+
     private final DcMotor leftFront;
     private final DcMotor leftRear;
     private final DcMotor rightFront;
     private final DcMotor rightRear;
+
+    private double speed = NORMAL_SPEED;
 
     private final Gamepad gamepad1;
     private final HardwareMap hardwareMap;
@@ -38,10 +43,15 @@ public class Drive {
 
     }
 
-    public void teleOpCommand(){
-        leftFront.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x);
-        leftRear.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x);
-        rightRear.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x);
-        rightFront.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x);
+    public void teleOpCommand() {
+        if (gamepad1.left_bumper) {
+            speed = SLOW_SPEED;
+        } else {
+            speed = NORMAL_SPEED;
+        }
+        leftFront.setPower((-gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x) * speed);
+        leftRear.setPower((-gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x) * speed);
+        rightRear.setPower((-gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x) * speed);
+        rightFront.setPower((-gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x) * speed);
     }
 }
