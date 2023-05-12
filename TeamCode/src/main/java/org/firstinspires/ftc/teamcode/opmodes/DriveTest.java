18335631eecb65b3382034b9be95d05cd9d6ac71
andrea <andreangonzalez59@gmail.com>
2023-05-12 10:56:25 -0500

    liner op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriveTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.subsystems.drive.MecanumDrive;
 
 @TeleOp
-public class driveTest extends OpMode {
+public class DriveTest extends OpMode {
 
     MecanumDrive mecanumDrive;
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMain.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.subsystems.Slide;
+import org.firstinspires.ftc.teamcode.subsystems.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.util.PoseStorage;
+
+@TeleOp
+public class TeleOpMain extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Claw claw = new Claw(this);
+        MecanumDrive drive = new MecanumDrive(this);
+        Slide slide = new Slide(this);
+        Arm arm = new Arm(this);
+
+        drive.setPoseEstimate(PoseStorage.currentPose);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            claw.teleOpCommand();
+            drive.mechDrive();
+            slide.teleOpCommand();
+            arm.testCommand();
+            telemetry.addData("IMU:", -drive.getRawExternalHeading());
+        }
+    }
+}
