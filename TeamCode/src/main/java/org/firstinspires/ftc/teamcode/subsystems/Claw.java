b7c49d92d3b72c61a008f30b4bb043d95e7503a3
andrea <andreangonzalez59@gmail.com>
2023-10-19 20:10:53 -0500

    addded file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -16,8 +18,9 @@ public class Claw {
 
     private double clawPosition = INITIAL_POSITION;
 
-//     create hardware variables
-    private final Servo claw;
+    //create hardware variables
+    private final CRServo clawOne;
+    private final CRServo clawTwo;
 
 
     private final HardwareMap hardwareMap;
@@ -32,35 +35,38 @@ public class Claw {
         gamepad1 = opMode.gamepad1;
         telemetry = opMode.telemetry;
 
-        claw = hardwareMap.get(Servo.class, "claw");
-        claw.setDirection(Servo.Direction.FORWARD);
+        clawOne = hardwareMap.get(CRServo.class, "clawOne");
+        clawTwo = hardwareMap.get(CRServo.class, "clawTwo");
+        clawOne.setDirection(CRServo.Direction.FORWARD);
+        clawTwo.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public void teleOpCommand() {
-
-        if (gamepad1.right_bumper) closeClaw();
-        if (gamepad1.left_bumper) openClaw();
-        if (gamepad2.dpad_down) openClaw();
+        if (gamepad2.right_bumper) intake();
+        if (gamepad2.left_bumper) outake();
     }
 
-    public void closeClaw(){
-        claw.setPosition(CLOSED);
+    public void intake() {
+        clawOne.setPower(1);
+        clawTwo.setPower(-1);
     }
 
-    public void openClaw(){
-        claw.setPosition(OPEN);
-    }
-
-
-    public void testCommand(){
-
-        if(gamepad1.a){
-            clawPosition += CHANGE_AMOUNT;
-        }
-        else if(gamepad1.b){
-            clawPosition -= CHANGE_AMOUNT;
-        }
-        claw.setPosition(clawPosition);
-        telemetry.addData("claw position", clawPosition);
+    public void outake() {
+        clawOne.setPower(-1);
+        clawTwo.setPower(1);
     }
 }
+
+
+//    public void testCommand(){
+//
+//        if(gamepad1.a){
+//            clawPosition += CHANGE_AMOUNT;
+//        }
+//        else if(gamepad1.b){
+//            clawPosition -= CHANGE_AMOUNT;
+//        }
+//        claw.setPosition(clawPosition);
+//        telemetry.addData("claw position", clawPosition);
+//    }
+//}
