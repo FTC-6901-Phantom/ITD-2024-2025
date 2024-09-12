25e88c77fbcea2c4846db86d88d13f04ec71a435
Daniel Cortez <cortedr658@gmail.com>
2024-09-11 22:13:35 -0500

    PAATHINGGGG

---
+++ MEEPMEEP/src/main/java/MEEp.java
@@ -0,0 +1,39 @@
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.Vector2d;
+
+public class MEEp {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(180), 10.67)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-33, -63, Math.PI / 2))
+                .setTangent(0)
+                .splineToConstantHeading(new Vector2d(-8, -40), Math.PI / 2)
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(-48, -40))
+                        .turn(Math.toRadians(135))
+                        .splineToConstantHeading(new Vector2d(-52,-52), Math.PI / 2)
+                        .waitSeconds(1)
+                .splineTo(new Vector2d(-58, -40), Math.PI / 2)
+                        .waitSeconds(0.5)
+                .turn(Math.toRadians(135))
+                        .strafeTo(new Vector2d(-52, -52))
+                        .waitSeconds(1)
+                        .turn(Math.toRadians(-135))
+                        .strafeTo(new Vector2d(36, -60))
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
